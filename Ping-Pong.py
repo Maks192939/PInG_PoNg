d885c5e9b2c4823522608e4373daf46a41269f58
Maxloldex_code <105293932+Maks192939@users.noreply.github.com>
2023-09-17 10:18:57 +0300

    Update Ping-Pong.py

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -31,7 +31,7 @@ class Player(GameSprite):
 
 
 #игровая сцена:
-back = (200, 255, 255) #цвет фона 
+back = (200, 255, 255) #фон
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
@@ -43,6 +43,21 @@ finish = False
 clock = time.Clock()
 FPS = 120
 
+#мяч и ракетки   
+racket1 = Player('platform2.png', 30, 200, 4, 70, 70) 
+racket2 = Player('platform1.png', 520, 200, 4, 70, 70)
+ball = GameSprite('ball.png', 200, 200, 6, 35, 35)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('Irpok 1 prougral', True, (180, 0, 0))
+lose2 = font.render('Irpok 2 prougral!', True, (180, 0, 0))
+
+
+speed_x = 3
+speed_y = 3
+
+
 while game:
    for e in event.get():
        if e.type == QUIT:
@@ -50,6 +65,38 @@ while game:
   
    if finish != True:
        window.blit(background, (0, 0))
+       racket1.update_l()
+       racket2.update_r()
+       ball.rect.x += speed_x
+       ball.rect.y += speed_y
+
+
+       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+           speed_x *= -1
+           speed_y *= 1
+      
+       #если мяч достигает границ экрана, меняем направление его движения
+       if ball.rect.y > win_height-50 or ball.rect.y < 0:
+           speed_y *= -1
+
+
+       #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+       if ball.rect.x < 0:
+           finish = True
+           window.blit(lose1, (200, 200))
+           game_over = True
+
+
+       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+       if ball.rect.x > win_width:
+           finish = True
+           window.blit(lose2, (200, 200))
+           game_over = True
+
+
+       racket1.reset()
+       racket2.reset()
+       ball.reset()
 
 
    display.update()
